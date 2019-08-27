117eda319e26b66e300164d2c7446df33699f092
paulorieck <paulo.rieck@prodgectool.com>
2019-08-27 09:15:42 -0300

    Development continuation

--- startup.js
+++ startup.js
@@ -2,25 +2,25 @@
 
 const os = require("os");
 
-if ( os.platform() === "win32" ) {
+/*if ( os.platform() === "win32" ) {
 
   const {exec} = require('child_process');
 
   const electron = exec("node "+__dirname+"/electron-builder-online-mac.js");
 
   electron.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+      console.log('stdout: '+data);
   });
 
   electron.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
+    console.log('stderr: '+data);
   });
 
   electron.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log('child process exited with code '+code);
   });
 
-} else if ( os.platform() === "darwin" || os.platform() === "linux" ) {
+} else if ( os.platform() === "darwin" || os.platform() === "linux" ) {*/
 
   const {spawn} = require('child_process');
 
@@ -34,16 +34,16 @@ if ( os.platform() === "win32" ) {
   const electron = spawn("node", args, options);
 
   electron.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
+      console.log('stdout: '+data);
   });
 
   electron.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
+    console.log('stderr: '+data);
   });
 
   electron.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
+    console.log('child process exited with code '+code);
   });
 
-}
+//}
 
