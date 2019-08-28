a3a46419562efb5000313229d0512905d7f2f0f0
paulorieck <paulo.rieck@prodgectool.com>
2019-08-28 10:51:25 -0300

    Development continuation

--- electron-builder-online-mac.js
+++ electron-builder-online-mac.js
@@ -65,7 +65,7 @@ function cloneGit(repository, execution_path, socket, callback) {
 
     const {spawn} = require('child_process');
 
-    var args = ["clone", "\""+repository+"\""];
+    var args = ["clone", repository];
 
     const options = {
         cwd: execution_path,
@@ -236,7 +236,7 @@ wss.on('connection', (socket, req) => {
             rimraf(path.join(tempDirectory, parameters.name), [], function () { // Removes directory
 
                 // Downloads the GIT repository content to the newly created repository
-                cloneGit(parameters.repository, path.join(tempDirectory, parameters.name), socket, function () {
+                cloneGit(parameters.repository, tempDirectory, socket, function () {
 
                     console.log("Clonned your repo succesfully!");
 
